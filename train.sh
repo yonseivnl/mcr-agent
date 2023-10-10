f9b4e1afe39fd1854c74dfd3191b182fb5a16614
Suvaansh <suvaansh2008bhambri@gmail.com>
2023-10-10 16:06:21 +0530

    Add training script

---
+++ train.sh
@@ -0,0 +1,75 @@
+
+################################## Train master policy ##################################
+cd MasterPolicy
+export ALFRED_ROOT=$(pwd)
+ln -s ../env .
+ln -s ../gen .
+ln -s ../exp .
+python MasterPolicy/models/train/train_seq2seq.py \
+				--dout exp/MasterPolicy \
+				--batch 16 \
+				--lr 1e-4 \
+				--gpu \
+				--save_every_epoch \
+				--panoramic \
+				--panoramic_concat 
+cd ..
+#########################################################################################
+
+
+############################# Training interaction policies #############################
+cd Interactions
+export ALFRED_ROOT=$(pwd)
+ln -s ../env .
+ln -s ../gen .
+ln -s ../exp .
+subgoals=(CleanObject HeatObject CoolObject SliceObject ToggleObject PickupObject PutObject)
+# Loop through each subgoal and execute the training command
+for subgoal in "${subgoals[@]}"; do
+    python models/train/train_seq2seq.py \
+        --subgoal_analysis=${subgoal} \
+        --dout exp/${subgoal} \
+        --batch 16 \
+        --lr 1e-3 \
+        --gpu \
+        --save_every_epoch
+done
+cd ..
+#########################################################################################
+
+
+########################## Train policy composition controller ##########################
+cd PCC
+export ALFRED_ROOT=$(pwd)
+ln -s ../env .
+ln -s ../gen .
+ln -s ../exp .
+python models/train/train_seq2seq.py \
+				--dout exp/PCC \
+				--batch 16 \
+				--lr 1e-3 \
+				--gpu \
+				--save_every_epoch \
+				--panoramic \
+				--panoramic_concat 
+cd ..
+#########################################################################################
+
+
+############################## Train Object Encoding Module #############################
+cd OEM
+export ALFRED_ROOT=$(pwd)
+ln -s ../env .
+ln -s ../gen .
+ln -s ../exp .
+python models/train/train_seq2seq.py \
+				--dout exp/OEM \
+				--batch 16 \
+				--lr 1e-3 \
+				--gpu \
+				--save_every_epoch \
+				--panoramic \
+				--panoramic_concat
+cd ..
+#########################################################################################
+
