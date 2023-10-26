dc93469170ec598158dbf62a1c1c0d62f4164040
Byeonghwi Kim <byeonghwikim@yonsei.ac.kr>
2023-10-26 13:11:16 +0900

    Delete extract_feature.sh

--- extract_feature.sh
+++
@@ -1,5 +0,0 @@
-export ALFRED_ROOT=$(pwd)
-CUDA_VISIBLE_DEVICES=9 python models/utils/extract_resnet.py	\
-	--batch 32	\
-	--filename feat_conv_onlyAutoAug4_panoramic.pt	\
-	--randomization auto_aug
